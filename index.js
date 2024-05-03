b2141cd2ad869ee6b1b288642f38be5f0dd27efe
MrBarker47 <jebarker7@gmail.com>
2024-05-03 17:34:07 -0400

    Update files

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-let darkMode = localStorage.getItem("checkbox");
-const toggleMode = document.querySelector("#checkbox");
-
-//check if dark is enabled
-// if it's enabled, turn it off
-// if it's disabled, turn it on
-
-const enableDarkMode = () => {
-  //1. add update darkMode to the body
-  document.body.classList.add("darkMode");
-  //2. update darkmode in the localStorage
-  localStorage.setItem("darkMode", "enabled");
-};
-
-const disableDarkMode = () => {
-  document.body.classList.remove("darkMode");
-  localStorage.setItem("darkMode", null);
-};
-
-if (darkMode === "enabled") {
-  enableDarkMode();
-}
-
-toggleMode.addEventListener("click", () => {
-  darkMode = localStorage.getItem("darkMode");
-
-  if (darkMode !== "enabled") {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
 });
