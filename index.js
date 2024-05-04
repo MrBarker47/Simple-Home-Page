a6b50013a39638eed8f0de2521ccd76914298460
MrBarker47 <jebarker7@gmail.com>
2024-05-04 17:25:45 -0400

    Update files

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let darkMode = localStorage.getItem("checkbox");
 const checkbox = document.getElementById("checkbox");
+
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
+
+const enableDarkMode = () => {
+  document.body.classList.add("darkMode");
+  localStorage.setItem("darkMode, enabled");
+};
